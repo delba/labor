37dae1dc87972536d579dd1b3037fc1e16dd5774
delba <delba@server.fake>
2013-09-02 17:32:15 +0200

    generate seeds for employee

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+path = File.join(Rails.root, 'data', 'employees.csv')
+options = { headers: true }
+
+CSV.foreach path, options do |row|
+  Employee.create!(row.to_hash)
+end

0fd5d2f4565cfb3d9b4b2d36a6c6cc00817141e2
delba <delba@server.fake>
2013-09-02 18:40:50 +0200

    geocode employee after validation

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,13 @@
 class Employee < ActiveRecord::Base
+  geocoded_by :address
+
+  after_validation :geocode, if: :address_changed?
+
+  def address
+    "#{street}, #{zip} #{city}, #{country}"
+  end
+
+  def address_changed?
+    street_changed? || city_changed? || zip_changed? || country_changed?
+  end
 end

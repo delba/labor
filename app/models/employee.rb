a73c5dbe14b86286aa55a8528b87f79bc16e1079
delba <delba@server.fake>
2013-09-02 19:12:43 +0200

    custom geocode to get formatted addresses

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -10,4 +10,24 @@ class Employee < ActiveRecord::Base
   def address_changed?
     street_changed? || city_changed? || zip_changed? || country_changed?
   end
+
+private
+
+  def geocode
+    return unless result = Geocoder.search(address)[0]
+    format_address(result)
+    set_coordinates(result)
+  end
+
+  def format_address(result)
+    self.street = result.street_address
+    self.zip = result.postal_code
+    self.city = result.city
+    self.country = result.country
+  end
+
+  def set_coordinates(result)
+    self.latitude = result.latitude
+    self.longitude = result.longitude
+  end
 end

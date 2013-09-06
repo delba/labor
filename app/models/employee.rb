962232e8193d838507999ce19bdcc101c472b0e3
delba <delba@server.fake>
2013-09-06 02:52:51 +0200

    custom geocode in a block

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,14 @@
 class Employee < ActiveRecord::Base
-  geocoded_by :address
+  geocoded_by :address do |obj, results|
+    if result = results.first
+      obj.street = result.street_address
+      obj.zip = result.postal_code
+      obj.city = result.city
+      obj.country = result.country
+      obj.latitude = result.latitude
+      obj.longitude = result.longitude
+    end
+  end
 
   after_validation :geocode, if: :address_changed?
 
@@ -9,25 +18,5 @@ class Employee < ActiveRecord::Base
 
   def address_changed?
     street_changed? || city_changed? || zip_changed? || country_changed?
-  end
-
-private
-
-  def geocode
-    return unless result = Geocoder.search(address)[0]
-    format_address(result)
-    set_coordinates(result)
-  end
-
-  def format_address(result)
-    self.street = result.street_address
-    self.zip = result.postal_code
-    self.city = result.city
-    self.country = result.country
-  end
-
-  def set_coordinates(result)
-    self.latitude = result.latitude
-    self.longitude = result.longitude
   end
 end
